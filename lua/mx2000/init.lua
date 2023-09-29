fcec87c156257c5d8e1f28576d729a86b0c189c7
max <max.noller@asd.de>
2023-09-29 15:30:56 +0200

    updated all the thigns

--- lua/mx2000/init.lua
+++ lua/mx2000/init.lua
@@ -1,2 +1,4 @@
 require("mx2000.remap")
 require("mx2000.set")
+
+vim.cmd("colorscheme carbonfox")
--- lua/mx2000/packer.lua
+++ lua/mx2000/packer.lua
@@ -12,4 +12,49 @@ use {
 -- or                            , branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
+
+-- use "folke/tokyonight.nvim",
+use "EdenEast/nightfox.nvim" -- Packer
+
+use({
+  "folke/trouble.nvim",
+  config = function()
+      require("trouble").setup {
+          icons = false,
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+      }
+  end
+})
+
+use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+use("tpope/vim-fugitive")
+use("nvim-treesitter/nvim-treesitter-context");
+
+
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+          -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+  }
+}
+use("github/copilot.vim")
 end)
