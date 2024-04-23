f965bbe858e87dafcf77a81d30911d89fd63c4c4
max <max.noller@asd.de>
2024-04-23 10:34:15 +0200

    update

---
+++ lua/mx2000/lazy/lsp.lua
@@ -0,0 +1,118 @@
+
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+
+    config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+                "pyright"
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+				    runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+                ["pyright"] = function()
+                    local lspconfig =  require("lspconfig")
+                    local util = require("lspconfig/util")
+                    local path = util.path
+                    local function get_python_path(workspace)
+                      -- Use activated virtualenv.
+                      if vim.env.VIRTUAL_ENV then
+                        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+                      end
+
+                      -- Find and use virtualenv in workspace directory.
+                        local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+                          if match ~= '' then
+                            local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+                            return path.join(venv, 'bin', 'python')
+                          end
+
+                      -- Fallback to system Python.
+                      return exepath('python3') or exepath('python') or 'python'
+                    end
+
+                    lspconfig.pyright.setup {
+                          capabilities = capabilities,
+                          on_attach = on_attach,
+                          before_init = function(_,config)
+                            config.settings.python.pythonPath = get_python_path(config.root_dir)
+                          end
+                        }
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
+}
